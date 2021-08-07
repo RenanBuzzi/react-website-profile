3f5fc4af0f39834ab3aff36acac85f6785edef93
RenanBuzzi <renangasparbuzzi@gmail.com>
2021-08-07 12:20:23 +0100

    test video mute and autoplay

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,19 +2,19 @@ import React from "react";
 import "../App.css";
 import { Button } from "./Button";
 import "./HeroSection.css";
-// import "./TypeMessage.css";
+import "./TypeMessage.css";
 
 const HeroSection = () => {
   return (
     <div className="hero-container">
       <div className="video">
         {/* <video src="/videos/Coding.mp4" autoPlay loop muted></video> */}
-        <video src="/videos/Coding.mp4" muted></video>
+        <video src="/videos/Coding.mp4" autoPlay muted></video>
       </div>
       <h1>Renan Buzzi</h1>
-      {/* <div class="typewriter">
+      <div class="typewriter">
         <h1>Software Engineer...</h1>
-      </div> */}
+      </div>
       <div className="hero-btns">
         <Button
           className="btns"
