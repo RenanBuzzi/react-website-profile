db69c94c64c698edd0ced239825cafb404feafe9
RenanBuzzi <renangasparbuzzi@gmail.com>
2021-08-14 18:56:58 +0100

    Contact plus playsinline

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -89,6 +89,7 @@ export default function Contact() {
             autoPlay={true}
             muted
             loop
+            playsinline
           ></video>
         </div>
         <div className="ContainerRight">
